800375224fccd9143135e5f7286c4fd551ee260a
aidinrakanov <altynaikanatbekova@gmail.com>
2020-02-08 20:36:41 +0600

    "2 commit"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-	private static Object Printable;
+	private static Printable printable;
 
 	public static void main(String[] args) {
 		createObject("Cars").print();
@@ -16,17 +16,17 @@ public class Main {
 	public static Printable createObject(String className) {
 		switch (className) {
 			case "Cars":
-				Printable = new Cars(500, "белый", "BMW");
+				printable = new Cars(500, "белый", "BMW");
 				break;
 			case "Dolls":
-				Printable = new Dolls(700, 80, "Barbie");
+				printable = new Dolls(700, 80, "Barbie");
 				break;
 			case "Balls":
-				Printable = new Balls(1000, 4, "Футбольный");
+				printable = new Balls(1000, 4, "Футбольный");
 				break;
 
 		}
-		return (Printable) Printable;
+		return printable;
 
 	}
 }
